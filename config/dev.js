8a19edd90759d9ec88914bf8b55936be46eef8d8
Itay Azani <itay.azani@gmail.com>
2021-11-30 12:52:13 +0200

    connect to mongo atlas DB

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-  // 'dbURL': 'mongodb+srv://or-herman:Oc123456@cluster0.irbwd.mongodb.net/gigDB?retryWrites=true&w=majority',
-  'dbURL': 'mongodb://localhost:27017',
+  'dbURL':'mongodb+srv://itay-finderr:itay1354@cluster0.ggaca.mongodb.net/gigDB?retryWrites=true&w=majority',
+
+  // 'dbURL': 'mongodb://localhost:27017',
 }
